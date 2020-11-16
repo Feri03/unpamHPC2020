32bce5cf1226f1ac13c20f8d90b15b8d29af790c
Tera <teramuza@gmail.com>
2020-11-16 22:09:25 +0700

    15 November 2020

--- react_native/aplikasiKU/App.js
+++ react_native/aplikasiKU/App.js
@@ -9,33 +9,16 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {StyleSheet, View, Text} from 'react-native';
-
-const HomePage = ({navigation}) => {
-  return (
-    <View style={Styles.container}>
-      <Text style={Styles.text} onPress={() => navigation.navigate('Detail')}>
-        {'Tekan untuk pindah screen'}
-      </Text>
-    </View>
-  );
-};
-
-const DetailPage = () => {
-  return (
-    <View style={Styles.container}>
-      <Text style={Styles.text}>{'Ini screen Stack kedua'}</Text>
-    </View>
-  );
-};
+import {HomePage, DetailPage} from './src/Routes/ScreenConfig';
+import {Routes} from './src/Routes';
 
 const App: () => React$Node = () => {
   const Stack = createStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomePage} />
-        <Stack.Screen name="Detail" component={DetailPage} />
+        <Stack.Screen name={Routes.Home} component={HomePage} />
+        <Stack.Screen name={Routes.Detail} component={DetailPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -50,19 +33,5 @@ const App: () => React$Node = () => {
   );
   */
 };
-
-const Styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: 300,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgb(234, 234, 235)',
-  },
-  text: {
-    fontSize: 18,
-    color: '#0b0c0f',
-  },
-});
 
 export default App;
---
+++ react_native/aplikasiKU/src/Screens/Home/Home.screen.component.js
@@ -0,0 +1,21 @@
+// @flow
+
+import React from 'react';
+import {Text, View} from 'react-native';
+import {Styles} from './Home.screen.style';
+import {Routes} from '../../Routes';
+
+const HomePage = ({navigation}) => {
+  const data = 'Selamat jalan -home';
+  return (
+    <View style={Styles.container}>
+      <Text
+        style={Styles.text}
+        onPress={() => navigation.navigate(Routes.Detail, {data})}>
+        {'Tekan untuk pindah screen'}
+      </Text>
+    </View>
+  );
+};
+
+export default HomePage;
